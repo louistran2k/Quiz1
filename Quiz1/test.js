a7cc722422c61d9717c637218fc873d805f4ecc9
louistran2k <louistran2k>
2022-05-20 08:59:45 +0700

    fix

--- Quiz1/test.js
+++ Quiz1/test.js
@@ -1,13 +1,13 @@
-function groupByLength(arr) {
-  const result = {};
-  arr.forEach(item => {
-    if(!result[item.length]){
-      result[item.length] = [item]
-    } else {
-      result[item.length].push(item)
-    }
-  })
-  return result;
-}
+// function groupByLength(arr) {
+//   const result = {};
+//   arr.forEach(item => {
+//     if(!result[item.length]){
+//       result[item.length] = [item]
+//     } else {
+//       result[item.length].push(item)
+//     }
+//   })
+//   return result;
+// }
 
-console.log(groupByLength(['one', 'two', 'three', 'four', 'five']));
+// console.log(groupByLength(['one', 'two', 'three', 'four', 'five']));
